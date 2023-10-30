bdb4982a8f20267aff9d4be27b9a9b8cccd484cb
Rikke Saust <rlsaust@gmail.com>
2023-10-30 15:40:46 +0100

    add variable for initial entries

--- src/components/EntriesSection/EntriesSection.jsx
+++ src/components/EntriesSection/EntriesSection.jsx
@@ -1,5 +1,19 @@
 import "./EntriesSection.css";
 
+const initialEntries = [{
+  motto:"That's life in the city",
+  date:'OCT 27, 2023',
+  text:'textf adfj ioa ejifiweiewif weifjweijfasdhcn wefwaeoiufewiou',
+  isBookmarked:false
+},  
+{
+  motto:"That's life in general",
+  date:'OCT 20, 2023',
+  text:'fodius  oadfi idoasfu adof uidasfyeiwryuwei  dsfy oau fiadou fua diofauf',
+  isBookmarked:true
+}]
+
+
 export default function EntriesSection ({children}) {
     return (
       <section className="entries">
